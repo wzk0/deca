960b3462c1db539db11ca90fcc248c3aecbf4767
wzk0 <729301277@qq.com>
2022-11-04 14:24:03 +0800

    提交!

--- list.py
+++ list.py
@@ -7,6 +7,7 @@ path=[]
 os.chdir('songs')
 for l in ls:
 	for ll in os.listdir(l):
-		path.append({'name':ll.replace('.mp3',''),'al':l,'url':'https://ghproxy.com/https://raw.githubusercontent.com/wzk0/deca/main/songs/%s/%s'%(l,ll),'cover':'https://ghproxy.com/https://raw.githubusercontent.com/wzk0/deca/main/songs/%s/cover.png'%l})
+		if ll!='cover.png':
+			path.append({'name':ll.replace('.mp3',''),'al':l,'url':'https://ghproxy.com/https://raw.githubusercontent.com/wzk0/deca/main/songs/%s/%s'%(l,ll),'cover':'https://ghproxy.com/https://raw.githubusercontent.com/wzk0/deca/main/songs/%s/cover.png'%l})
 with open('list.json','w')as f:
 	f.write(json.dumps(path,ensure_ascii=False))
