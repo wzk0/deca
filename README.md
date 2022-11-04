bda3c0f6e7a5cd0573790ac2d27945b995c3c733
wzk0 <729301277@qq.com>
2022-11-04 14:07:01 +0800

    提交!

---
+++ README.md
@@ -0,0 +1,9 @@
+## Deca Joins专辑收录
+
+> 音源来自网易云.
+
+类似的项目有:
+
+* [nj-lizhi/song](https://github.com/nj-lizhi/song) - 收录李志专辑
+
+**侵删**.
---
+++ list.py
@@ -0,0 +1,12 @@
+import os
+import json
+
+ls=os.listdir('songs')
+ls.remove('list.json')
+path=[]
+os.chdir('songs')
+for l in ls:
+	for ll in os.listdir(l):
+		path.append({'name':ll.replace('.mp3',''),'al':l,'url':'https://ghproxy.com/https://raw.githubusercontent.com/wzk0/deca/main/songs/%s/%s'%(l,ll),'cover':'https://ghproxy.com/https://raw.githubusercontent.com/wzk0/deca/main/songs/%s/cover.png'%l})
+with open('list.json','w')as f:
+	f.write(json.dumps(path,ensure_ascii=False))
